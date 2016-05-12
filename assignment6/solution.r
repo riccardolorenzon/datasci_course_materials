ae3cb004a0789f9e31bc304c40dcc70cbad50e20
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2016-05-13 00:12:11 +0200

    # of incidents by time

---
+++ assignment6/solution.r
@@ -0,0 +1,10 @@
+# how do incidents vary by time of day?
+
+x <- sea$Occurred.Date.or.Date.Range.Start
+hr <- as.integer(substr(x, 12,13))
+AP <- substr(x, 21, 22)
+AP[AP=="PM"]=12
+AP[AP=="AM"]=0
+AP <- as.integer(AP)
+sea$Hr24 <- as.factor(AP+hr)
+plot(sea$Hr24)
